2e4a556d77fa66007b8b28f181ce36d2b0f0b3bd
aalexebr <alex.ebr@icloud.com>
2023-06-26 17:17:19 +0200

    ADD- creates new card from input

--- js/script.js
+++ js/script.js
@@ -31,46 +31,47 @@ const team = [
     }
 ];
 console.log(team)
-const ul = document.createElement('ul');
-document.body.append(ul);
-
+// const ul = document.createElement('ul');
+// document.body.append(ul);
+createCard();
 // let card = `<div class="card">
 //                 <div class="img"></div>
 //                 <h3 class="name"></h3>
 //                 <h4 class="role"></h4>
 //             </div>`;
+function createCard (){
 
-for(let i=0; i < team.length; i++){
-    // console.log(team[i])
-    let card = `<div class="card">
-                    <div class="img"></div>
-                    <h3 class="name">${i}</h3>
-                    <h4 class="role"></h4>
-                 </div>`;
-    document.querySelector('.row').innerHTML += card
-    let allImg = document.querySelectorAll('.img')
-    let allName = document.querySelectorAll('.name')
-    let allRole = document.querySelectorAll('.role')
-    let teamMember = team[i]
-    for(let key in teamMember){
-        
-        // let li = document.createElement('li');
-        // ul.append(li);
-        console.log(key,teamMember[key])
-        if(key == 'img'){
-            // li.innerHTML = `<img src="img/${teamMember[key]}">`; 
-            allImg[i].innerHTML = `<img src="img/${teamMember[key]}">`; 
-        }
-        else if(key == 'name'){
-            allName[i].innerHTML =  teamMember[key]
-            // li.innerHTML = `${key}: ${teamMember[key]}`;  
-        }
-        else if ( key == 'role'){
-            allRole[i].innerHTML = teamMember[key]
+    for(let i=0; i < team.length; i++){
+        // console.log(team[i])
+        let card = `<div class="card">
+                        <div class="img"></div>
+                        <h3 class="name">${i}</h3>
+                        <h4 class="role"></h4>
+                    </div>`;
+        document.querySelector('.row').innerHTML += card
+        let allImg = document.querySelectorAll('.img')
+        let allName = document.querySelectorAll('.name')
+        let allRole = document.querySelectorAll('.role')
+        let teamMember = team[i]
+        for(let key in teamMember){
+            
+            // let li = document.createElement('li');
+            // ul.append(li);
+            console.log(key,teamMember[key])
+            if(key == 'img'){
+                // li.innerHTML = `<img src="img/${teamMember[key]}">`; 
+                allImg[i].innerHTML = `<img src="img/${teamMember[key]}">`; 
+            }
+            else if(key == 'name'){
+                allName[i].innerHTML =  teamMember[key]
+                // li.innerHTML = `${key}: ${teamMember[key]}`;  
+            }
+            else if ( key == 'role'){
+                allRole[i].innerHTML = teamMember[key]
+            };
         };
     };
-};
-
+}
 const AddMember = document.getElementById('add-member')
 
 AddMember.addEventListener('submit',
@@ -86,5 +87,7 @@ AddMember.addEventListener('submit',
         };
         team.push(newMember)
         console.log('added',team)
+        document.querySelector('.row').innerHTML = ''
+        createCard();
     }
 )
