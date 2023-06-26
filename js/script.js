118324cb90152ff67aa269c7049c2d363dd254c2
aalexebr <alex.ebr@icloud.com>
2023-06-26 15:34:41 +0200

    initializing

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const team = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        img : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        img : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        img : 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        img : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name : 'Scott Estrada',
+        role : 'Developer',
+        img : 'scott-estrada-developer.jpg',
+    },
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        img : 'barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+for(let i=0; i < team.length; i++){
+    // console.log(team[i])
+    let teamMember = team[i]
+    for(let key in teamMember){
+        console.log(key,teamMember[key])
+    }
+}
